526387ed68d1da683d3c42f93072a9777d791699
Filipe Leandro <lealves.work@gmail.com>
2022-03-16 01:40:37 -0300

    Produto e associacao muitos para muitos

--- src/main/java/com/alves/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/alves/cursomc/resources/CategoriaResource.java
@@ -1,13 +1,10 @@
 package com.alves.cursomc.resources;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.alves.cursomc.domain.Categoria;
@@ -20,7 +17,8 @@ public class CategoriaResource {
 	@Autowired
 	private CategoriaService service;
 	
-	@RequestMapping(value="/{id}",method=RequestMethod.GET)
+	//@RequestMapping(value="/{id}",method=RequestMethod.GET)
+	@GetMapping(value="/{id}")
 	public ResponseEntity<?> find(@PathVariable Integer id) {
 		Categoria obj = service.buscar(id);
 		return ResponseEntity.ok().body(obj);
