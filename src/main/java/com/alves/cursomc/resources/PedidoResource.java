6cfd6c10272011c21b996b328107bf106b4020ba
Filipe Leandro <lealves.work@gmail.com>
2022-03-17 17:14:12 -0300

    Atualizando com PUT, outros pequenos ajustes

--- src/main/java/com/alves/cursomc/resources/PedidoResource.java
+++ src/main/java/com/alves/cursomc/resources/PedidoResource.java
@@ -19,8 +19,8 @@ public class PedidoResource {
 	
 	//@RequestMapping(value="/{id}",method=RequestMethod.GET)
 	@GetMapping(value="/{id}")
-	public ResponseEntity<?> find(@PathVariable Integer id) {
-		Pedido obj = service.buscar(id);
+	public ResponseEntity<Pedido> find(@PathVariable Integer id) {
+		Pedido obj = service.find(id);
 		return ResponseEntity.ok().body(obj);
 		
 	}
