de5109913e5719c49a58404e4db81d965880d99a
Filipe Leandro <lealves.work@gmail.com>
2022-03-20 20:40:03 -0300

    Listando todas categorias com DTO

--- src/main/java/com/alves/cursomc/services/CategoriaService.java
+++ src/main/java/com/alves/cursomc/services/CategoriaService.java
@@ -1,5 +1,6 @@
 package com.alves.cursomc.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -43,4 +44,8 @@ public class CategoriaService {
 			throw new DataIntegrityException("Não é possível excluir uma categoria que possui produtos!");
 		}
 	}
+	public List<Categoria> findAll() {
+		return repo.findAll();
+		}
 }
+
